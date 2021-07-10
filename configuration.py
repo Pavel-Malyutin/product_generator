7e2e1e506ffe506864a404bba1b7d4cd6865d639
User <wer@qer.3>
2021-07-10 21:22:52 +0300

    added config.py

---
+++ configuration.py
@@ -0,0 +1,16 @@
+﻿FEATURES = ('Память', 'Экран', 'Стандарт связи', 'АКБ', 'Камера', 'Корпус', )
+
+CATEGORIES = ('Смартфон',)
+
+with open('input/brands.txt') as f:
+    BRANDS = f.read().splitlines()
+
+with open('input/names.txt') as f:
+    NAMES = f.read().splitlines()
+
+FEATURES_DICT = {}
+FILE_NUM = 1
+for v in FEATURES:
+    with open(f'input/feature_{FILE_NUM}.txt', encoding='UTF-8') as file:
+        FEATURES_DICT[v] = file.read().splitlines()
+    FILE_NUM += 1
