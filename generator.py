62e5d42a8252057cf702dd8a9f6d3037a126bf63
User <wer@qer.3>
2021-07-12 18:32:09 +0300

    added generator

--- generator.py
+++ generator.py
@@ -1,22 +1,38 @@
+import pandas as pd
+
 from configuration import *
 import random
 import string
+import shutil
+
+used_slugs = []
 
 
-def rand_gen():
+def rand_gen(len_to_gen):
     alphabet_string = string.ascii_uppercase
     name = ''
-    for i in range(5):
+    global used_slugs
+    for i in range(len_to_gen):
         name = name + random.choice(alphabet_string + str(random.randint(0, 100)))
-    return name
+    if name not in used_slugs:
+        used_slugs.append(name)
+        return name
+    else:
+        rand_gen(len_to_gen)
 
 
 class Product:
     def __init__(self):
-        self.features = {'Бренд': str(random.choice(BRANDS)).capitalize(),
-                         'Имя': f'{str(random.choice(NAMES)).capitalize()}' + '-' + f'{rand_gen()}',
-                         'Категория': str(random.choice(CATEGORIES)).capitalize(),
-                         'Цена': random.randint(5000, 100000)}
+        self.slug = str(f'{rand_gen(15)}').lower()
+        # shutil.copy(random.choice(PHOTOS), f'output/{self.slug}.jpg')
+        self.features = {'Категория': str(random.choice(CATEGORIES)).capitalize(),
+                         'Имя': f'{str(random.choice(NAMES)).capitalize()}' + '-' + f'{rand_gen(4)}',
+                         'Slug': f'{self.slug}',
+                         'Image': f'{self.slug}.jpg',
+                         'Описание': DESCRIPTION,
+                         'Цена': random.randint(5000, 100000)//100*100,
+                         'Бренд': str(random.choice(BRANDS)).capitalize(),
+                         }
         for feature in FEATURES:
             self.features[feature] = random.choice(FEATURES_DICT[feature])
 
--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import pandas as pd
 from generator import Product
 from configuration import ITEMS_TO_GEN
 
-
 if __name__ == '__main__':
-    pass
-
+    counter = 0
+    items = pd.DataFrame()
+    while counter < ITEMS_TO_GEN:
+        p = Product()
+        items = items.append(pd.DataFrame(p.features.values(), index=p.features.keys()))
+        # print(p.features)
+        counter += 1
